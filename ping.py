4521fedc45be5f412b0e5a5e18f50df134c8436c
Fastweb <102059143+FastwebIT@users.noreply.github.com>
2026-07-05 01:21:40 +0200

    Delete ping.py

--- ping.py
+++
@@ -1,6 +0,0 @@
-from telethon import events
-
-def setup(client):
-    @client.on(events.NewMessage(outgoing=True, pattern=r"\.ping"))
-    async def ping_handler(event):
-        await event.edit("🏓 **Pong!**")
